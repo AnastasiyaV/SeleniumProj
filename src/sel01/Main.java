55292adbac93a517785567af680b4d88870bb998
AnastasiyaV <anastasiya.vynogradska@hotmail.com>
2021-03-15 21:59:34 +0200

    Home task Selenium1 remamed into Sel01

--- src/sel01/Main.java
+++ src/sel01/Main.java
@@ -1,10 +1,9 @@
-package sel02;
+package sel01;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
+
 import java.util.Iterator;
 import java.util.Set;
 //        Buttons (additional task: click by position)
